f25c84351b95a6a79f673553b9cd147d23fc39ee
Lumacel <luiscelestre74@gmail.com>
2025-03-08 23:51:48 -0300

    primer commit

---
+++ prueba_langchain.py
@@ -0,0 +1,18 @@
+from langchain_community.document_loaders import CSVLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import os
+
+data_dir = 'data'
+
+# Iterar sobre los archivos en el directorio de datos
+for filename in os.listdir(data_dir):
+    if filename.endswith('.csv'):
+        file_path = os.path.join(data_dir, filename)
+        loader = CSVLoader(file_path, encoding="utf-8")
+        documentos = loader.load()
+        print("TYPE:", type(documentos))
+
+
+for i,doc in enumerate(documentos, start=1):
+    print(f"documento {i}: \n", doc)
+    
